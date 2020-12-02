e0a10f6f9f0a6b41e18d2ad10d6684700564a8d7
Tamara Islam Meghla <tamara.meghla@tuni.fi>
2020-12-03 01:28:34 +0600

    endsWith.test.js implemented

---
+++ test/endsWith.test.js
@@ -0,0 +1,32 @@
+// Importing require so it works on Node.js >14
+import {
+    createRequire
+} from 'module';
+const require = createRequire(
+    import.meta.url);
+    // Chai assert
+    const assert = require('chai').assert;
+    
+    import endsWith from "../src/endsWith.js";
+    
+    /****** 16.endsWith.js tests *****/
+    
+    describe("endsWith.js tests", function () {
+        const array = "abc";
+        describe("Should return a boolean describing, if a given string ends with another given string", function () {
+            it("should return true when endsWith('abc', 'c') ", function () {
+                const expectedResult = true;
+                assert.deepEqual(endsWith("abc", "c"), expectedResult);
+            });
+            
+            it("should return false when endsWith('abc', 'b')", function () {
+                const ExpectedResult = false;
+                assert.deepEqual(endsWith("abc", "b"), ExpectedResult);
+            });
+            
+            it("should return true when endsWith('abc', 'b', 2)", function () {
+                const expectedOutput = true;
+                assert.deepEqual(endsWith("abc", "b", 2), expectedOutput)
+            });
+        });
+    });
