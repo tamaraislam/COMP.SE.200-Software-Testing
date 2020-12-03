1e06e3e0c1eaa739ab41ad07ee70a84f4f5465ab
Tamara Islam Meghla <tamara.meghla@tuni.fi>
2020-12-03 19:43:47 +0600

    isBoolean.test.js implemented

---
+++ test/isBoolean.test.js
@@ -0,0 +1,29 @@
+// Importing require so it works on Node.js >14
+import {
+    createRequire
+} from 'module';
+const require = createRequire(
+    import.meta.url);
+    // Chai assert
+    const assert = require('chai').assert;
+    
+    import isBoolean from "../src/isBoolean.js";
+    
+    /****** 23.isBoolean.js tests *****/
+    
+    describe("isBoolean.js tests", function () {
+        
+        describe("Should Return `true` if `value` is a boolean, else `false`", function () {
+            it("should return true if `value` is `false` ", function () {
+                assert.strictEqual(isBoolean(false),true);
+            });
+            
+            it("Should Return `false` if `value` is `null`", function () {
+                assert.strictEqual(isBoolean(null),false);
+            });
+            
+            it("Should Return `false` if `value` is string", function () {
+                assert.strictEqual(isBoolean("true"),false);
+            });
+        });
+    });
