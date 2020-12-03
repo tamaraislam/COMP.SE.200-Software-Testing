f4ce18e02aa44004e44b2d34e4dc9bb3554839b7
Tamara Islam Meghla <tamara.meghla@tuni.fi>
2020-12-03 16:25:55 +0600

    filter.test.js

---
+++ test/filter.test.js
@@ -0,0 +1,33 @@
+// Importing require so it works on Node.js >14
+import {
+    createRequire
+} from 'module';
+const require = createRequire(
+    import.meta.url);
+    // Chai assert
+    const assert = require('chai').assert;
+    
+    import filter from "../src/filter.js";
+    
+    /****** 19.filter.js tests *****/
+    
+    describe("filter.js tests", function () {
+        
+        describe("Should iterate over elements of a given array & should return a new array for which the given predicate returns “truthy", function () {
+            it("should return true when filter([true, true, false], Boolean)", function () {
+                const testData1 = [true, true, false];
+                const expectedResult = [true,true];
+                assert.deepEqual(filter(testData1,Boolean), expectedResult);
+            });
+            
+            it("should return object for  ['barney'] when filter([{ 'user': 'barney', 'active': true },{ 'user': 'fred', 'active': false } ], active)", function () {
+                const testData2 = [{ 'user': 'barney', 'active': true },{ 'user': 'fred', 'active': false } ];
+                const ExpectedResult = [{ 'user': 'barney', 'active': true }];
+                assert.deepEqual(filter(testData2,({ active }) => active), ExpectedResult);
+            });
+            
+            it("should return an array ", function () {
+                assert.typeOf(filter(), "array");
+            });
+        });
+    });
