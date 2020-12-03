ed6f04520cda072a110190f0416dbebc83b00137
Tamara Islam Meghla <tamara.meghla@tuni.fi>
2020-12-03 21:20:13 +0600

    isLength.test.js implemented

---
+++ test/isLength.test.js
@@ -0,0 +1,38 @@
+// Importing require so it works on Node.js >14
+import {
+    createRequire
+} from 'module';
+const require = createRequire(
+    import.meta.url);
+    // Chai assert
+    const assert = require('chai').assert;
+    
+    import isLength from "../src/isLength.js";
+    
+    /****** 26.isLength.js tests *****/
+    
+    describe("isLength.js tests", function () {
+        
+        describe("Should return `true` if `value` is a valid length, else `false`", function () {
+            it("should return true if `value` is 3", function () {
+                assert.strictEqual(isLength(3),true);
+            });
+            
+            it("Should return true  if `value` is  Max-safe-integer ", function () {
+                const MAX_SAFE_INTEGER = 9007199254740991;
+                assert.strictEqual(isLength(MAX_SAFE_INTEGER),true);
+            });
+            
+            it("Should return false if `value` is infinity", function () {
+                assert.strictEqual(isLength(Infinity),false);
+            });
+            
+            it("Should return false if `value` is '3'", function () {
+                assert.strictEqual(isLength("3"),false);
+            });
+            
+            it("Should return false if `value` is less than -1", function () {
+                assert.strictEqual(isLength(-1),false);
+            });
+        });
+    });
